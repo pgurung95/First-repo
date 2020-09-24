f8f2efbaed2fc81661198d4710fc4cf77a23c62a
pgurung95 <69608183+pgurung95@users.noreply.github.com>
2020-09-24 00:02:51 -0400

    Update script.js

--- WorkDayScheduler/script.js
+++ WorkDayScheduler/script.js
@@ -19,36 +19,6 @@ var timeList = [
     "5 PM"
 ];
 
-var idTime = [
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17"
-];
-
-for (var i = 0; i < timeList.length; i++) {
-    var newRow = $("<div class='row time-block'>").attr("id", idTime[i]);
-    var newDiv2 = $("<div class='hour col-1'>")
-    var newDiv = $("<textarea class='col-10'>");
-    var newButt = $("<button type='button' class='saveBtn col-1 far fa-save'>");
-
-    timeBlocks.append(newRow);
-
-    newDiv2.text(timeList[i]);
-    newRow.append(newDiv2);
-
-    newDiv.text();
-    newRow.append(newDiv);
-
-    newButt.text();
-    newRow.append(newButt);
-}
-
 localStorageFunction();
 
 function localStorageFunction() {
